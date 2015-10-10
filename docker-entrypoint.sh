14b5329333519fef3f717de6b35ab0b8ce2769ae
Alban Montaigu <alban.montaigu@gmail.com>
2015-10-10 14:52:16 +0200

    Better entrypoint information

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -e
 
+# Who and where am I ?
+echo >&2 "[INFO] ---------------------------------------------------------------"
+echo >&2 "[INFO] GLOBAL INFORMATIONS"
+echo >&2 "[INFO] ---------------------------------------------------------------"
+echo >&2 "[INFO] whoami : $(whoami)"
+echo >&2 "[INFO] pwd : $(pwd)"
+
 # Backup the prev install in case of fail...
-tar -zcf /var/backup/freshrss/freshrss-v$(date '+%y%m%d%H%M%S').tar.gz /var/www
+echo >&2 "[INFO] ---------------------------------------------------------------"
+echo >&2 "[INFO] Backup old freshrss installation in $(pwd)"
+echo >&2 "[INFO] ---------------------------------------------------------------"
+tar -zcvf /var/backup/freshrss/freshrss-v$(date '+%Y%m%d%H%M%S').tar.gz .
+echo >&2 "[INFO] Complete! Backup successfully done in $(pwd)"
 
 # Since freshrss can be upgraded by overwriting files do the upgrade !
 # @TODO use VERSION file to check if necessary
@@ -10,11 +21,16 @@ tar -zcf /var/backup/freshrss/freshrss-v$(date '+%y%m%d%H%M%S').tar.gz /var/www
 # 
 # File copy strategy taken from wordpress entrypoint
 # @see https://github.com/docker-library/wordpress/blob/master/fpm/docker-entrypoint.sh
-echo >&2 "Installing or upgrading freshrss in $(pwd) - copying now..."
-find -maxdepth 1 ! -regex '^\./data.*$' ! -regex '^\.$' -exec rm -rf {} +
-tar cf - --one-file-system -C /usr/src/freshrss . | tar xf -
-chown -R nginx:nginx ./
-echo >&2 "Complete! FreshRSS has been successfully installed / upgraded to $(pwd)"
+echo >&2 "[INFO] ---------------------------------------------------------------"
+echo >&2 "[INFO] Installing or upgrading freshrss in $(pwd) - copying now..."
+echo >&2 "[INFO] ---------------------------------------------------------------"
+echo >&2 "[INFO] Removing old installation"
+find -maxdepth 1 ! -regex '^\./data.*$' ! -regex '^\.$' -exec rm -rvf {} +
+echo >&2 "[INFO] Extracting new installation"
+tar cvf - --one-file-system -C /usr/src/freshrss . | tar xvf -
+echo >&2 "[INFO] Fixing rights"
+chown -Rfv nginx:nginx .
+echo >&2 "[INFO] Complete! FreshRSS has been successfully installed / upgraded to $(pwd)"
 
 # Exec main command
 exec "$@"
